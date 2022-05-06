a09b3d9b938c92baab72f39feade8cc02e0a4e3c
rafik ben <bendjelloulrafik@gmail.com>
2022-05-06 15:47:26 +0200

    jhgghhjghjgjhhj

--- src/main/java/src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		//final int index;
 		for(int i=0; i<=6; i++) {
-			System.out.println("Hello bugs gs!");
+			System.out.println("Hello bugs ss!");
 		}
 	}
 }
